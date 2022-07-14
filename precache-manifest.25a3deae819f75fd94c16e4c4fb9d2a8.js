26642e04fa5b26e99fbbc2a671dc0a6d68692ff3
Tharik J.H <darshinifiles@gmail.com>
2022-07-15 03:08:09 +0800

    Updates

---
+++ precache-manifest.25a3deae819f75fd94c16e4c4fb9d2a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2e7358199605983d3d601ab88b818a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7a8fbcad4595e8d2cb9",
+    "url": "/static/css/main.3ac2b9af.chunk.css"
+  },
+  {
+    "revision": "5c9ff16ec5bee154aee2",
+    "url": "/static/js/2.5286435d.chunk.js"
+  },
+  {
+    "revision": "a7a8fbcad4595e8d2cb9",
+    "url": "/static/js/main.236c47a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
